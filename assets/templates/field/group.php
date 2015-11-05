ee8c3bf74236f4f3b2e4b23a7134c229ced6db95
StephenMurphy <smurphy@crowdfavorite.com>
2015-11-05 16:36:44 -0700

    Allow for optional settings group information -- can be generated on the fly. Allow for simple array-of-fields-by-section arguments for streamlined settings setup. Standards updates and minor correction to the click-to-edit behavior. Update group template to use the new callback structure. Plugin is now in minimal usable state. Needs documentation and field verification but a properly constructed array will run smoothly and most fatal misconstructions will error gracefully.

--- assets/templates/field/group.php
+++ assets/templates/field/group.php
@@ -5,12 +5,10 @@ if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
 ?>
 <table class="field-group">
 <?php foreach ( $args['multi'] as $sub_field ) : ?>
-	<tr class="sub-field">
-		<td>
-	<?php
-		$sub_field['renderer']( $sub_field );
-	?>
-		</td>
+	<tr class="js-top">
+		<td class="field-group-entry"><?php
+			$sub_field['renderer']( $sub_field );
+		?></td>
 	</tr>
 <?php endforeach; ?>
 </table>
